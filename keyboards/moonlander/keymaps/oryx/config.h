30cabd9dae2869ee9c6e9a20972868cd7acde6c3
Nathan Bernard <natedawg1013@gmail.com>
2022-02-28 09:09:38 +0000

    web config update

--- keyboards/moonlander/keymaps/oryx/config.h
+++ keyboards/moonlander/keymaps/oryx/config.h
@@ -1,3 +1,14 @@
-#pragma once
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 125
 
-#include "../default/config.h"
+#define RETRO_TAPPING
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define CAPS_LOCK_STATUS
+#define FIRMWARE_VERSION u8"DA0lE/BQLyx"
+#define RGB_MATRIX_STARTUP_SPD 60
